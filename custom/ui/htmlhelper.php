f8366194c999fbc830b8e0df3203145c1e84a466
oidawtf <ILoveBJC@hotmail.com>
2013-10-23 00:25:54 +0200

    easteregg

--- custom/ui/htmlhelper.php
+++ custom/ui/htmlhelper.php
@@ -38,6 +38,51 @@ class htmlhelper {
         //$document->addStyleSheet(htmlhelper::getRoot()."media/system/css/bootstrap.css");
     }
     
+    public static function easteregg() {
+        $input = array();
+        
+        $orange = new ingredient();
+        $orange->name = "Hey Apple!";
+        $orange->description = "
+            Hey, hey, Apple, hey!<br />
+            Whoa! You're the biggest orange I've ever seen!<br />
+            I'm not a grape, I'm an orange!<br />
+            TOE-MAY-TOE!<br />
+            Wanna hear the most annoying sound in the world?<br />
+            I wish for a pot of gold!<br />
+            What a jip-sy.<br />
+            You're an apple!<br />
+            Hey! Are you related to Ms. Fortune Cookie?<br />
+            Hey, guys, what's so funny?<br />
+            Poe-Tay-Toe?<br />
+            I'm bored<br />
+            Yuck-yuck-yuck-yuck-yuck-yuck.<br /> 
+            ";
+        $orange->image = "/dietme/images/easteregg/easteregg_hey_apple_orange.jpg";
+        $orange->amount = "???";
+        $orange->calories = "???";
+        $input[] = $orange;
+        
+        $apple = new ingredient();
+        $apple->name = "What? What? What is it?!";
+        $apple->description = "
+            Yeah, that joke was funny the first four hundred times you said it.<br />
+            (sarcastically) Yeah, that was hilarious.<br />
+            What!?!?<br />
+            (confused) What kind of question is that? I don't even have arms; how am I going to do one push-up?<br />
+            No, stop it!<br />
+            Okay, you've made your point! Stop it.....<br />
+            Would you please be quiet?!<br />
+            For crying out loud, would you stop yammering for longer than three seconds? I can't even hear myself think! (grunts)<br />
+            ";
+        $apple->image = "/dietme/images/easteregg/easteregg_hey_apple_apple.jpg";
+        $apple->amount = "???";
+        $apple->calories = "???";
+        $input[] = $apple;
+        
+        caloriesTable::show($input);
+    }
+    
     public static function image($src, $class = NULL, $alt = NULL) {
        
         htmlhelper::initialize();
